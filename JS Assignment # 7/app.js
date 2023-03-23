e786eee51ccc11d22dce49e853ff2931f62322b9
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-03-24 01:03:28 +0500

    Add files via upload

---
+++ JS Assignment # 7/app.js
@@ -0,0 +1,64 @@
+// *** JS Assignment # 7 *** \\
+
+// Question # 1
+
+// var num = prompt("Enter any positive Integer: ")
+// console.log("Number: " + num)
+
+// console.log("Round off Value: " + Math.round(num))
+// console.log("Floor Value of the Number: " + Math.floor(num))
+// console.log("Ciel Value of the Number: " + Math.ceil(num))
+
+// Question # 2
+
+// var negativeNum = prompt("Enter any positive Integer: ")
+// console.log("Number: " + negativeNum)
+
+// console.log("Round off Value: " + Math.round(negativeNum))
+// console.log("Floor Value of the Number: " + Math.floor(negativeNum))
+// console.log("Ciel Value of the Number: " + Math.ceil(negativeNum))
+
+// Question # 3
+
+// var num = -4
+// var absNum = (num < 0) ? -num : num;
+// console.log("The Absolute value of -4 is: " + absNum)
+
+// var num = prompt("Enter a dice number: ")
+
+// Question # 4
+
+// var num = 6
+// alert("Random Dice value is: " + Math.ceil(Math.random() * num))
+
+// Question # 5
+
+// var toss = prompt("Please tell head or tail?")
+// if (toss === "head") {
+//     alert(Math.floor(Math.random() * 2))
+// }
+// if (toss === "tail") {
+//     alert(Math.floor(Math.random() * 2))
+// }
+
+// Question # 6
+
+// var num = 100
+// console.log("Random Number between 1 and 100 is: " + Math.ceil(Math.random() * num))
+
+// Question # 7
+
+// let weight = prompt("What is your weight?");
+// document.write("Your weight is " + parseFloat(weight) + " kilograms.");
+
+
+// Question # 8
+
+// var ask = prompt("Enter secret Number")
+//     // var secretNum = 4
+// var anas = Math.floor(Math.random() * 10)
+// if (ask == anas) {
+//     alert("congratulation You won")
+// } else {
+//     alert("try again")
+// }
